939a2718f93b9c42542e10f748c38185aaebd1ec
AbhinavSanthosh <abhinavsanthoshmathew8@gmail.com>
2024-05-01 10:07:58 +0530

    update vite config

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -35,7 +35,7 @@ const Navbar = () => {
     >
       <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
         <Link
-          to='/'
+          to='/portfolio'
           className='flex items-center gap-2'
           onClick={() => {
             setActive("");
@@ -45,7 +45,7 @@ const Navbar = () => {
           <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
           <p className='text-white text-[18px] font-bold cursor-pointer flex '>
             Abhinav &nbsp;
-            <span className='sm:block hidden'> | </span>
+            <span className='sm:block hidden'> </span>
           </p>
         </Link>
 
